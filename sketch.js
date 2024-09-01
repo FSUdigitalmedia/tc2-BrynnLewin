f0dcf86dd2b9594908f7c664b767b34b974e878c
Brynn Lewin <bhl21@fsu.edu>
2024-09-01 14:01:31 -0400

    I'm Done

--- sketch.js
+++ sketch.js
@@ -1,7 +1,24 @@
 function setup() {
-  createCanvas(400, 400);
+  //created a canvas 600 pixels wide
+  //and 400 pixels high
+  createCanvas(600, 400);
 }
 
 function draw() {
-  background(125);
+  //sky blue backgrounf
+  background(135, 206, 235);
+  //sun in top-right corner
+  fill("yellow")
+  stroke("orange")
+  strokeWeight(20)
+  circle(550, 50, 100);
+  //grass on bottom half
+  fill("green")
+  stroke(0)
+  strokeWeight(1)
+  rect(0, 200, 600, 200)
+  //emojis
+  textSize(75)
+  text("🌸", 100, 250); //flower
+  text ("🐞", mouseX, mouseY); //ladybug interactive
 }
